ccb7e33ded0ffcadb76870ea1f9b43badf5b7392
Itstf <favarelli.thaiza@gmail.com>
2022-08-16 11:27:56 -0300

    commit

---
+++ src/Components/Home.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Navbar from './Navbar';
+import Slider from './Slider/Slider';
+
+class Home extends Component {
+    state = {  } 
+    render() { 
+        return (
+            <>
+                <Navbar />
+            </>
+        );
+    }
+}
+ 
+export default Home;
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import '../navbar.css';
+import Slider from './Slider/Slider';
+
+function Navbar() {
+    return (  
+        <>
+            <div id='navbar'>
+                <div className='flex'>
+                    <div>
+                        <a href='#' className='logo'>
+                            <img src="logo_white.png" alt="" className='img-logo'/>
+                        </a>
+                    </div>
+                    <div>
+                        <button  className='button-login'> Login </button>
+                    </div>
+                </div>
+                <div>
+                    <Slider/>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default Navbar;
